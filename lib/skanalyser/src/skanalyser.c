44fbc6be5d9c371d3bd0b43f2d6ef8693acb81d3
shiauka <eric741216@gmail.com>
2015-07-28 12:51:13 +0800

    add dump function

--- lib/skanalyser/src/skanalyser.c
+++ lib/skanalyser/src/skanalyser.c
@@ -8,17 +8,14 @@
 
 static bool _Dump_price(FILE *pfinputfile, unsigned int count)
 {
-    if (pfinputfile == NULL && count != 0)
+    if (pfinputfile == NULL || count == 0)
         return false;
 
     SK_PRICE *price = NULL;
 
-    printf("count = %d\n",count);
-
     price = malloc(sizeof(SK_PRICE)*count+1);
     if (price == NULL)
         return false;
-    printf("count = %d\n",count);
 
     if (fread((char *)price, sizeof(SK_PRICE), count, pfinputfile) == 0)
     {
@@ -26,8 +23,6 @@ static bool _Dump_price(FILE *pfinputfile, unsigned int count)
         return false;
     }
         
-        printf("count = %d\n",count);
-
     int index = 0;
 
     for (index = 0; index< count; index++)
@@ -42,6 +37,92 @@ static bool _Dump_price(FILE *pfinputfile, unsigned int count)
     free(price);
     return true;
 }
+
+static bool _Dump_dividend(FILE *pfinputfile, unsigned int count)
+{
+    if (pfinputfile == NULL || count == 0)
+        return false;
+
+    SK_DIVIDEND *dividend = NULL;
+
+    dividend = malloc(sizeof(SK_DIVIDEND)*count+1);
+    if (dividend == NULL)
+        return false;
+
+    if (fread((char *)dividend, sizeof(SK_DIVIDEND), count, pfinputfile) == 0)
+    {
+        free(dividend);
+        return false;
+    }
+        
+    int index = 0;
+
+    for (index = 0; index< count; index++)
+    {
+        printf("%03d %0.02f %0.02f %0.02f %0.02f %0.02f\n",
+            dividend[index].year, dividend[index].cash, dividend[index].profit, 
+            dividend[index].surplus,dividend[index].stock,dividend[index].total);
+    }
+    free(dividend);
+    return true;
+}
+
+static bool _Dump_earning_month(FILE *pfinputfile, unsigned int count)
+{
+    if (pfinputfile == NULL || count == 0)
+        return false;
+
+    SK_EARNING_MONTH *month = NULL;
+
+    month = malloc(sizeof(SK_EARNING_MONTH)*count+1);
+    if (month == NULL)
+        return false;
+
+    if (fread((char *)month, sizeof(SK_EARNING_MONTH), count, pfinputfile) == 0)
+    {
+        free(month);
+        return false;
+    }
+        
+    int index = 0;
+
+    for (index = 0; index< count; index++)
+    {
+        printf("%03d-%02d %d\n",
+            month[index].year,month[index].month,month[index].income);
+    }
+    free(month);
+    return true;
+}
+
+static bool _Dump_earning_season(FILE *pfinputfile, unsigned int count)
+{
+    if (pfinputfile == NULL || count == 0)
+        return false;
+
+    SK_EARNING_SEASON *season= NULL;
+
+    season = malloc(sizeof(SK_EARNING_SEASON)*count+1);
+    if (season == NULL)
+        return false;
+
+    if (fread((char *)season, sizeof(SK_EARNING_SEASON), count, pfinputfile) == 0)
+    {
+        free(season);
+        return false;
+    }
+        
+    int index = 0;
+    for (index = 0; index< count; index++)
+    {
+        printf("%03d-%02d  %d, %d\n",
+            season[index].year,season[index].season,
+            season[index].pretax_income, season[index].aftertax_income);
+    }
+    free(season);
+    return true;
+}
+
 
 bool SKApi_SKANALYSER_Dump(const char *Inputfile)
 {
@@ -76,8 +157,20 @@ bool SKApi_SKANALYSER_Dump(const char *Inputfile)
     {
         case SK_DATA_TYPE_PRICE:
             _Dump_price(pfinputfile, header.datacount);
+            break;  
+            
+        case SK_DATA_TYPE_DIVIDEND:
+             _Dump_dividend(pfinputfile, header.datacount);
+            break;      
+            
+        case SK_DATA_TYPE_EARNING_MONTH:
+            _Dump_earning_month(pfinputfile, header.datacount);
             break;
-
+            
+        case SK_DATA_TYPE_EARNING_SEASON:
+            _Dump_earning_season(pfinputfile, header.datacount);
+            break;
+            
         default:
             printf("Code: %d has incorrect type",header.code);
             break;
