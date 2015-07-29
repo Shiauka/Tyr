9447219b86e2432e4f5cc48524f75337424c09c8
shiauka <eric741216@gmail.com>
2015-07-30 00:05:41 +0800

    refine para check

--- lib/skanalyser/src/main.c
+++ lib/skanalyser/src/main.c
@@ -2,22 +2,20 @@
 #include "SKcommon.h"
 #include "skanalyser.h"
 
-int main(int argc, char *argv[])
+
+static bool checkparanum(int cmd, int arg)
 {
-    if (argc < 3)
-    {
-        SKApi_SKANALYSER_Help();
-        return 0;
-    }
-
     bool bRet = false;
-    int _intcommand = atoi(argv[1]);
-
-
-    switch (_intcommand)
+    switch (cmd)
     {   
         case SKANALYSER_TYPE_DUMP: 
-            bRet = SKApi_SKANALYSER_Dump(argv[2]);
+            if (arg == 3)
+                bRet = true;
+            break;
+
+        case SKANALYSER_TYPE_ANALYSIS:
+            if (arg == 4)
+                bRet = true;
             break;
             
         default:
@@ -26,7 +24,44 @@ int main(int argc, char *argv[])
 
     if (!bRet)
     {
-        printf("Error CVS2SK\n");
+        printf("You use the wrong cmd, please follow below list\n");
+        SKApi_SKANALYSER_Help();
+    }
+    return bRet;
+}
+    
+
+int main(int argc, char *argv[])
+{
+    bool bRet = false;
+    int _intcommand = SKANALYSER_TYPE_MAX;
+
+    /*assinged cmd*/
+    if (argv[1] !=NULL)
+        _intcommand = atoi(argv[1]);
+
+    /*check input para with diff cmd*/
+    if (!checkparanum(_intcommand,argc))
+        return 0;
+
+    /*start function*/
+    switch (_intcommand)
+    {   
+        case SKANALYSER_TYPE_DUMP: 
+            bRet = SKApi_SKANALYSER_Dump(argv[2]);
+            break;
+
+        case SKANALYSER_TYPE_ANALYSIS:
+            printf("to be continue\n");
+            break;
+            
+        default:
+            break;
+    }
+
+    if (!bRet)
+    {
+        printf("sk analysis function error\n");
         return 0;
     }
     
